d98018d82b0bf0e82be4411efd699c8ad2e6f0ee
Olga Dubavets <o.dubavets@gmail.com>
2024-08-15 22:40:26 +0200

    Added more logic to buyWeapon function

--- freecodecamp/js/rolePlayingGame/role_playing_game.js
+++ freecodecamp/js/rolePlayingGame/role_playing_game.js
@@ -99,7 +99,7 @@ function buyHealth() {
 }
 
 function buyWeapon() {
-    if (currentWeaponIndex < weapons.length) {
+    if (currentWeaponIndex < weapons.length - 1) {
         if (gold >= 30) {
             gold -= 30;
             currentWeaponIndex++;
@@ -111,6 +111,10 @@ function buyWeapon() {
         } else {
             text.innerText = "You do not have enough gold to buy a weapon.";
         }
+    } else {
+        text.innerText = "You already have the most powerful weapon!";
+        button2.innerText = "Sell weapon for 15 gold";
+        button2.onclick = sellWeapon;
     }
 }
 
