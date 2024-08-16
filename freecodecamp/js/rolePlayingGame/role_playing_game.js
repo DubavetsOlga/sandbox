1d13d83431bf1f741d081fa0d6b311727d3e9ef8
Olga Dubavets <o.dubavets@gmail.com>
2024-08-16 22:42:47 +0200

    Added goFight logic

--- freecodecamp/js/rolePlayingGame/role_playing_game.js
+++ freecodecamp/js/rolePlayingGame/role_playing_game.js
@@ -25,6 +25,12 @@ const locations = [
         "button functions": [fightSlime, fightBeast, goTown],
         text: "You enter the cave. You see some monsters."
     },
+    {
+        name: "fight",
+        "button text": ["Attack", "Dodge", "Run"],
+        "button functions": [attack, dodge, goTown],
+        text: "You are fighting a monster."
+    }
 ];
 
 const button1 = document.querySelector("#button1");
@@ -148,7 +154,11 @@ function fightDragon() {
 }
 
 function goFight() {
-
+    update(locations[3]);
+    monsterHealth = monsters[fighting].health;
+    monsterStats.style.display = 'block';
+    monsterName.innerText = monsters[fighting].name;
+    monsterHealthText.innerText = monsterHealth;
 }
 
 function attack() {
