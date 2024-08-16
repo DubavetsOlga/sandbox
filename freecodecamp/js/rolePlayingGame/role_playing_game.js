5565dfd0d14805745e231e9e35ea0ac77cc9484f
Olga Dubavets <o.dubavets@gmail.com>
2024-08-16 22:32:26 +0200

    Step 110. Added const monsters

--- freecodecamp/js/rolePlayingGame/role_playing_game.js
+++ freecodecamp/js/rolePlayingGame/role_playing_game.js
@@ -56,6 +56,12 @@ const weapons = [
     },
 ];
 
+const monsters = [
+    {name: "slime", level: 2, health: 15},
+    {name: "fanged beast", level: 8, health: 60},
+    {name: "dragon", level: 20, health: 300}
+];
+
 //initialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
