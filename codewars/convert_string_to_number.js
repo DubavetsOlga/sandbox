bfba9f04b69ecade9891855adf425b1b35e8dbbb
Olga Dubavets <o.dubavets@gmail.com>
2024-08-17 10:59:07 +0200

    Resolve

---
+++ codewars/convert_string_to_number.js
@@ -0,0 +1,7 @@
+//Description
+//We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+//SOLUTION
+const stringToNumber = function(str){
+    return parseInt(str);
+}
