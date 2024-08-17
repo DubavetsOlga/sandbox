60dbc40be855bf50d84f69ebe4e61bf7dc1d4818
Olga Dubavets <o.dubavets@gmail.com>
2024-08-17 11:06:10 +0200

    Resolve

---
+++ codewars/abbreviate_two_word_name.js
@@ -0,0 +1,8 @@
+//DESCRIPTION:
+//Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+//The output should be two capital letters with a dot separating them.
+
+//SOLUTION
+function abbrevName(name){
+    return name.split(' ').map(el => el[0].toUpperCase()).join('.');
+}
