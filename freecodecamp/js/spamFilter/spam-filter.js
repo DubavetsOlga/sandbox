07c488b8adf6b1c3bab5de11271f289ea17d92be
Olga Dubavets <o.dubavets@gmail.com>
2024-08-21 19:31:20 +0200

    Step 11

--- freecodecamp/js/spamFilter/spam-filter.js
+++ freecodecamp/js/spamFilter/spam-filter.js
@@ -2,6 +2,7 @@ const messageInput = document.getElementById('message-input');
 const result = document.getElementById('result');
 const checkMessageButton = document.getElementById('check-message-btn');
 const helpRegex = /please help|assist me/i;
+const denyList = [helpRegex];
 
 const isSpam = (msg) => helpRegex.test(msg);
 
