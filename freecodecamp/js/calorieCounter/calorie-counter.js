c34ececc789b6d9d6eb6df5e31974272470b7346
Olga Dubavets <o.dubavets@gmail.com>
2024-09-23 22:59:00 +0200

    Step 40

--- freecodecamp/js/calorieCounter/calorie-counter.js
+++ freecodecamp/js/calorieCounter/calorie-counter.js
@@ -19,5 +19,6 @@ function isInvalidInput(str) {
 }
 
 function addEntry() {
-    let targetId = "#" + entryDropdown.value;
+    const targetId = "#" + entryDropdown.value;
+    const targetInputContainer = document.querySelector(targetId + ' .input-container');
 }
